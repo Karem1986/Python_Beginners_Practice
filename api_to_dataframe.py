06889fbc7d894b15bc77a8212a2a55d93753688a
karin.ortiz <karin.ortiz@movares.nl>
2025-06-10 19:39:15 +0200

    Converting the bitcoind data to pandas dataframe for better user experience.

--- api_to_dataframe.py
+++ api_to_dataframe.py
@@ -4,9 +4,17 @@ from pycoingecko import CoinGeckoAPI
 from datetime import datetime
 
 cg = CoinGeckoAPI()
+
 bitcoin_data = cg.get_coin_market_chart_by_id(id='bitcoin', vs_currency='usd', days=30)
 
-# convert the 'prices' data to a DataFrame
-prices_df = pd.DataFrame(bitcoin_data['prices'], columns=['timestamp', 'price'])
+# # Create a DataFrame from the prices
+prices = pd.DataFrame(bitcoin_data['prices'], columns=['timestamp', 'price'])
+# Convert the timestamp to a readable date format
+prices['date'] = prices['timestamp'].apply(lambda x: datetime.fromtimestamp(x / 1000).strftime('%Y-%m-%d %H:%M:%S'))
 
+print(prices)
 
+# # Drop the timestamp column
+prices.drop(columns=['timestamp'], inplace=True)
+
+print(prices)
